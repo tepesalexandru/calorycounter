e6ae04bb29de35a44bea650e01f2a761a00773c8
LupusDiamond <godzplayer@hotmail.com>
2020-05-21 11:46:36 +0300

    fixed sizing issue

--- app/components/ListItemLabels/ListItemLabels.js
+++ app/components/ListItemLabels/ListItemLabels.js
@@ -4,23 +4,22 @@ import { View, Text, StyleSheet } from "react-native";
 const ListItemLabels = ({ item, deleteItem }) => {
   return (
     <View style={styles.Labels}>
-        <Text style={styles.LabelText}>Food Items</Text>
-        <Text style={styles.LabelText}>Calories</Text>
+      <Text style={styles.LabelText}>Food Items</Text>
+      <Text style={styles.LabelText}>Calories</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   Labels: {
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
   },
   LabelText: {
     color: "#718096",
-    fontWeight: "600"
-  }
+    fontWeight: "600",
+  },
 });
 
 export default ListItemLabels;
